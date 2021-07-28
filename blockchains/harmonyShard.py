de29ec14d898e4b30093ea30a8317bbcf97bd9f9
xTeKc <bcd3v@pm.me>
2021-07-28 19:18:49 -0400

    refactor

--- blockchains/harmonyShard.py
+++ blockchains/harmonyShard.py
@@ -11,6 +11,3 @@ hs0_ca = config('HARMONY_SHARD_API_KEY')
 
 def harmony_shard_contract_address(hs0_ca):
     input("Enter Harmony Shard Contract Address: ")
-
-
-harmony_shard_contract_address(hs0_ca)
