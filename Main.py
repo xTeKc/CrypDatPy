059273aac7c48f2132f9366ba1675ec39267b48e
xTeKc <bcd3v@pm.me>
2021-07-28 18:22:29 -0400

    import other modules

--- Main.py
+++ Main.py
@@ -5,3 +5,12 @@ import requests
 import selenium
 from decouple import config
 
+import Blockchains/Binance
+import Blockchains/Cardano
+import Blockchains/Ethereum
+import Blockchains/Fantom
+import Blockchains/HarmonyShard
+import Blockchains/Huobi
+import Blockchains/Polygon
+import Blockchains/Solana
+import Blockchains/xDai
