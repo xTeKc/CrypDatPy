4bc1f92c935aa902dfefa96b12dda006378ef513
xTeKc <bcd3v@pm.me>
2021-07-28 18:48:11 -0400

    refactor

--- blockchains/ethereum.py
+++ blockchains/ethereum.py
@@ -6,15 +6,11 @@ import selenium
 from decouple import config
 
 
-eth_ca = config('ETHEREUM_API_KEY')
-
-eth_search = input('Enter Ethereum Contract Address: ')
-
-response = requests.get(f'https://api.etherscan.io/api?module=contract&action=getsourcecode&address={eth_search}&apikey={eth_ca}')
-
-
-def ethereum_contract_address(eth_ca, response):
+def ethereum_contract_address():
+    eth_ca = config('ETHEREUM_API_KEY')
+    eth_search = input('Enter Ethereum Contract Address: ')
+    response = requests.get(f'https://api.etherscan.io/api?module=contract&action=getsourcecode&address={eth_search}&apikey={eth_ca}')
     print(response.json())
 
 
-ethereum_contract_address(eth_ca, response)
+ethereum_contract_address()
