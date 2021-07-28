12f7ea4aa7c9617592265e554b4a8ea71da1997e
xTeKc <bcd3v@pm.me>
2021-07-28 18:49:18 -0400

    refactor

--- blockchains/fantom.py
+++ blockchains/fantom.py
@@ -6,15 +6,11 @@ import selenium
 from decouple import config
 
 
-ftm_ca = config('FANTOM_API_KEY')
-
-ftm_search = input('Enter Fantom Contract Address: ')
-
-response = requests.get(f'https://api.ftmscan.com/api?module=contract&action=getsourcecode&address={ftm_search}&apikey={ftm_ca}')
-
-
-def fantom_contract_address(ftm_ca, response):
+def fantom_contract_address():
+    ftm_ca = config('FANTOM_API_KEY')
+    ftm_search = input('Enter Fantom Contract Address: ')
+    response = requests.get(f'https://api.ftmscan.com/api?module=contract&action=getsourcecode&address={ftm_search}&apikey={ftm_ca}')
     print(response.json())
 
 
-fantom_contract_address(ftm_ca, response)
+fantom_contract_address()
