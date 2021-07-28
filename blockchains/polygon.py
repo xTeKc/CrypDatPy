fff782d14e61c457fb2a0223a28a27bfe43eecce
xTeKc <bcd3v@pm.me>
2021-07-28 18:58:44 -0400

    refactor

--- blockchains/polygon.py
+++ blockchains/polygon.py
@@ -6,15 +6,11 @@ import selenium
 from decouple import config
 
 
-poly_ca = config('POLYGON_API_KEY')
-
-poly_search = input('Enter Polygon Contract Address: ')
-
-response = requests.get(f'https://api.polygonscan.com/api?module=contract&action=getsourcecode&address={poly_search}&apikey={poly_ca}')
-
-
-def polygon_contract_address(poly_ca, response):
+def polygon_contract_address():
+    poly_ca = config('POLYGON_API_KEY')
+    poly_search = input('Enter Polygon Contract Address: ')
+    response = requests.get(f'https://api.polygonscan.com/api?module=contract&action=getsourcecode&address={poly_search}&apikey={poly_ca}')
     print(response.json())
 
 
-polygon_contract_address(poly_ca, response)
+polygon_contract_address()
