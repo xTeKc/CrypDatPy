9266cd87eaef32cf58af87f11ae26d205cb3f8d6
xTeKc <bcd3v@pm.me>
2021-07-28 18:45:55 -0400

    refactor

--- blockchains/binance.py
+++ blockchains/binance.py
@@ -6,18 +6,14 @@ import selenium
 from decouple import config
 
 
-bsc_ca = config('BINANCE_API_KEY')
-
-bsc_search = input('Enter Binance Contract Address: ')
-
-response = requests.get(f'https://api.bscscan.com/api?module=contract&action=getsourcecode&address={bsc_search}&apikey={bsc_ca}')
-
-pretty_json = json.loads(response.text)
-
-def binance_contract_address(bsc_ca, response):
+def binance_contract_address():
+    bsc_ca = config('BINANCE_API_KEY')
+    bsc_search = input('Enter Binance Contract Address: ')
+    response = requests.get(f'https://api.bscscan.com/api?module=contract&action=getsourcecode&address={bsc_search}&apikey={bsc_ca}')
+    pretty_json = json.loads(response.text)
     print(json.dumps(pretty_json))
 
 
-binance_contract_address(bsc_ca, response)
+binance_contract_address()
 
 
