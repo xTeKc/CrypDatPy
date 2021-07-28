96ae929da52c9648fb4b1450890ce2e9fc7a05b1
xTeKc <bcd3v@pm.me>
2021-07-28 18:50:23 -0400

    refactor

--- blockchains/huobi.py
+++ blockchains/huobi.py
@@ -6,15 +6,11 @@ import selenium
 from decouple import config
 
 
-huo_ca = config('HUOBI_API_KEY')
-
-huo_search = input('Enter Huobi Contract Address: ')
-
-response = requests.get(f'https://api.hecoinfo.com/api?module=contract&action=getsourcecode&address={huo_search}&apikey={huo_ca}')
-
-
-def huobi_contract_address(huo_ca, response):
+def huobi_contract_address():
+    huo_ca = config('HUOBI_API_KEY')
+    huo_search = input('Enter Huobi Contract Address: ')
+    response = requests.get(f'https://api.hecoinfo.com/api?module=contract&action=getsourcecode&address={huo_search}&apikey={huo_ca}')
     print(response.json())
 
 
-huobi_contract_address(huo_ca, response)
+huobi_contract_address()
