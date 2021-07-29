7f7700a5ec380209608c49861db1f2256abee84e
xTeKc <bcd3v@pm.me>
2021-07-28 21:54:43 -0400

    refactor

--- main.py
+++ main.py
@@ -13,16 +13,22 @@ import blockchains.huobi
 import blockchains.polygon
 import blockchains.solana
 import blockchains.xDai
-from pycoingecko import CoinGeckoAPI
 
-cg = CoinGeckoAPI()
 
-# blockchains.binance.binance_contract_address()
-# blockchains.ethereum.ethereum_contract_address()
-# blockchains.fantom.fantom_contract_address()
-# blockchains.huobi.huobi_contract_address()
-# blockchains.polygon.polygon_contract_address()
+#     binance = blockchains.binance.binance_contract_address()
+#     ethereum = blockchains.ethereum.ethereum_contract_address()
+#     fantom = blockchains.fantom.fantom_contract_address()
+#     huobi = blockchains.huobi.huobi_contract_address()
+#     polygon = blockchains.polygon.polygon_contract_address()
 
+
+def coin_info():
+    id = input('Enter Crypto ID: ')
+    coin_by_id = requests.get(f'https://api.coingecko.com/api/v3/coins/{id}')
+    coin_info = json.loads(coin_by_id.text)
+    print(json.dumps(coin_info))
+
+coin_info()
 
 
 
