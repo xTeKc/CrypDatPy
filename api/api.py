ff506d1440566e89a3fd999f1b288d3a3307774a
xTeKc <bcd3v@pm.me>
2021-07-29 22:45:44 -0400

    add get method

--- api/api.py
+++ api/api.py
@@ -5,16 +5,26 @@ app = Flask(__name__)
 api = Api(app)
 
 
+class Contract(Resource):
+    def get(self, id=0):
+        if id == 0:
+            return random.choice(cryptocurrcies), 200
+        for contract in cryptocurrcies:
+            if(contract["id"] == id):
+                return contract, 200
+        return "Contract not found", 404
+
+
 cryptocurrcies = [
     {
         "id": "uni-eth",
         "name": "uniswap-eth",
-        "contract_address": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
+        "contract": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
     },
     {
         "id": "uni-bsc",
         "name": "uniswap-bsc",
-        "contract_address": "0xbf5140a22578168fd562dccf235e5d43a02ce9b1"
+        "contract": "0xbf5140a22578168fd562dccf235e5d43a02ce9b1"
     }
 ]
 
