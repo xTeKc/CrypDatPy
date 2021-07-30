644c73a32195f934eb1cbf64792435dded31abe5
xTeKc <bcd3v@pm.me>
2021-07-29 22:56:32 -0400

    add comments

--- api/api.py
+++ api/api.py
@@ -6,6 +6,8 @@ api = Api(app)
 
 
 class Contract(Resource):
+    
+    #Get specific Contract by ID or get Random ID if ID not specified
     def get(self, id=0):
         if id == 0:
             return random.choice(cryptocurrcies), 200
@@ -13,6 +15,23 @@ class Contract(Resource):
             if(contract["id"] == id):
                 return contract, 200
         return "Contract not found", 404
+    
+    #Ability to Add new Contract
+    def post(self, id):
+      parser = reqparse.RequestParser()
+      parser.add_argument("name")
+      parser.add_argument("contract")
+      params = parser.parse_args()
+      for contract in cryptocurrcies:
+          if(id == contract["id"]):
+              return f"Contract with id {id} already exists", 400
+      contract = {
+          "id": int(id),
+          "name": params["name"],
+          "contract": params["contract"]
+      }
+      cryptocurrcies.append(contract)
+      return contract, 201
 
 
 cryptocurrcies = [
