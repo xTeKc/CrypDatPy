050aa248d35cd6a1a19e5711375a604d3bad83fe
xTeKc <bcd3v@pm.me>
2021-08-23 22:40:25 -0400

    create init layout

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -1,2 +1,8 @@
 import PySimpleGUI as sg
 
+#define window contents
+layout = [[sg.Text("Enter Contract Address")],
+          [sg.Input(key="-INPUT-")],
+          [sg.Text(size=(40,1), key="-OUTPUT-")],
+          [sg.Button("Search"), sg.Button("End")]
+          ]
