d2ef48e6e98c66dc08ace7c400ed8ff2f715bc7a
xTeKc <bcd3v@pm.me>
2021-08-23 22:40:54 -0400

    refactor

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -4,5 +4,4 @@ import PySimpleGUI as sg
 layout = [[sg.Text("Enter Contract Address")],
           [sg.Input(key="-INPUT-")],
           [sg.Text(size=(40,1), key="-OUTPUT-")],
-          [sg.Button("Search"), sg.Button("End")]
-          ]
+          [sg.Button("Search"), sg.Button("End")]]
