ff271fffacc4a1a477c697463d8312eda22195fe
xTeKc <bcd3v@pm.me>
2021-08-23 22:41:54 -0400

    create window

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -5,3 +5,6 @@ layout = [[sg.Text("Enter Contract Address")],
           [sg.Input(key="-INPUT-")],
           [sg.Text(size=(40,1), key="-OUTPUT-")],
           [sg.Button("Search"), sg.Button("End")]]
+
+#create window
+window = sg.Window("CrypDat", layout)
